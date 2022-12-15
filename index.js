24e9604341d262bd4fea91d425609c40f350cce3
Dina Iyanuloluwa <88966134+iyanuloluwa-Miracle@users.noreply.github.com>
2022-12-15 03:07:10 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+let items = ["Buy Food", "Go to the gym", "Cook Food"];
+
+
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+  let today = new Date();
+
+  let options =  {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  }
+  let day = today.toLocaleDateString("en-Us",options)
+   res.render("list", {kindOfDay: day, newListItems: items})
+});
+
+app.post('/', (req,res)=>{
+  
+  let item = req.body.newItem
+  items.push(item);
+  
+  res.redirect("/")
+})
+
+app.listen(3000, () => {
+  console.log('server started');
+});
