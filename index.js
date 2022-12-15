8a42acb2155684a999ec07e755a83cc067c25055
Dina Iyanuloluwa <88966134+iyanuloluwa-Miracle@users.noreply.github.com>
2022-12-15 03:23:10 +0000

    added work route

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 let items = ["Buy Food", "Go to the gym", "Cook Food"];
-
+let workItems = [];
 
 
 app.set('view engine', 'ejs');
@@ -29,6 +29,16 @@ app.post('/', (req,res)=>{
   items.push(item);
   
   res.redirect("/")
+})
+
+.app.get('/', (req, res) => {
+  res.render('list', {listTitle:"Work List", newListItems: workItems});
+
+})
+app.post("/work", function(req, res){
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
 })
 
 app.listen(3000, () => {
