96f2c710bdc96ded028cb5faa624e3e56d895bfa
iyanuloluwa-Miracle <iyanudina@gmail.com>
2022-12-24 16:04:02 +0100

    my latest commit

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ app.post("/delete", function (req, res) {
 
 //using unknown parameters to navigate
 app.get("/:customListName", function (req, res) {
-  const customListName = req.params.customListName;
+  const customListName = _.capitalize(req.params.customListName);
 
   List.findOne({ name: customListName }, function (err, foundList) {
     if (!err) {
